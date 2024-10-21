f80192aac62b9f683afe61923f0332ef7c39f9a9
rohankhanal14 <rohankhanal114@gmail.com>
2024-10-21 23:02:28 +0545

    3-tire_app

---
+++ backend/db/connection.js
@@ -0,0 +1,24 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const URI = "mongodb+srv://root:toor@cluster0.huzi3n6.mongodb.net/";
+const client = new MongoClient(URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (err) {
+  console.error(err);
+}
+
+let db = client.db("employees");
+
+export default db;
